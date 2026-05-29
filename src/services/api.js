62f19e37bae4ec17e114a0f806d33f40669712cb
VidyaDarane <vidyadarane@caryanamindia.in>
2026-05-29 18:48:56 +0530

    login, register, upload documents and preview documents api integration done

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,20 @@
 // src/services/api.js
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api", // backend base URL
-  headers: { "Content-Type": "application/json" },
+  baseURL: "http://localhost:8081/api",
 });
 
 // Attach JWT token automatically
 api.interceptors.request.use((config) => {
+
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
